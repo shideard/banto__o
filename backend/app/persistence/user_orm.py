797d9f94ce356338530b4bf23889f26fc148642d
shideard <ardiani_deshi@apps.ipb.ac.id>
2026-05-11 17:52:59 +0700

    refine lagi

--- backend/app/persistence/user_orm.py
+++ backend/app/persistence/user_orm.py
@@ -23,8 +23,8 @@ class UserORM(Base):
 
     id       = Column(Integer, primary_key=True, index=True)
     nama     = Column(String(100), nullable=False)
-    email    = Column(String(100), unique=True, nullable=False)
-    password = Column(String(255), nullable=False)   # TODO: hash dengan bcrypt saat auth diimplementasi
+    email    = Column(String(100), unique=True, index=True, nullable=False)
+    password = Column(String(255), nullable=False)
     role     = Column(String(20), nullable=False)    # 'mahasiswa' | 'staf'
 
     # Mahasiswa fields
